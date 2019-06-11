c88dd1745c0f5a7e4f92b949d94aae8573b1a6a5
Sebastian Wetzel <seba.wetzel@gmail.com>
2019-06-10 23:43:18 -0300

    heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port:process.env.PORT  || 8888,
-    db: process.env.MONGODB || 'mongodb://127.0.0.1:27017/lingerie',
+    db: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lingerie',
     SECRET_TOKEN : 'C5B3AAEAC9CA6DF451BFE9D1F210C060B01D616560808B51C17AD79A651570BB'
 }
