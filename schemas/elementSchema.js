77da9934c8366ae2d6be27bb42463b5bdc4b7298
Sebastian Wetzel <seba.wetzel@gmail.com>
2019-06-16 01:05:45 -0300

    add a todo item

--- schemas/elementSchema.js
+++ schemas/elementSchema.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const ElementsSchema = Schema({
     //TODO add a user fild with the user id owner
+    //TODO add a timestamp fild for the last modification 
     name: String,
     type: { type: String, enum: ['unit', 'lineal', 'area'] },
     price: { type: Number, default: 0 },
